e4fef8eaa148a92a179855130502a8e1cc800c8c
joaomariussi <joaomariussi10@gmail.com>
2024-01-05 09:36:47 -0300

    :construction: Criando as novas rotas.
    - Criando um rota /sobre-nos.
    
    - Criando um rota /contato.

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return 'Olá, tudo certo?';
+});
+
+Route::get('/sobre-nos', function () {
+    return 'Sobre nós';
+});
+
+Route::get('/contato', function () {
+    return 'Contato';
 });
