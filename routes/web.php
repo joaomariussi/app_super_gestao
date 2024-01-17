25a6b165cdcf4d4dfaa44387df875a371c7c6f57
joaomariussi <joaomariussi10@gmail.com>
2024-01-17 09:26:37 -0300

    :rocket: Implementando algumas imagens e modificações nas views.

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'PrincipalController@principal');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
 
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 
-Route::get('/contato', 'ContatoController@contato');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+
+Route::get('/login', function () { return 'Login';})->name('site.login');
+
+
+Route::prefix('/app')->group(function () {
+    Route::get('/clientes', function () { return 'Clientes'; })->name('app.clientes');
+
+    Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
+
+    Route::get('/produtos', function () { return 'Produtos'; })->name('app.produtos');
+});
+
+Route::fallback(function () {
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">Clique aqui</a> para ir para a página inicial';
+});
+
+
