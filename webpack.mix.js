f79e7914e2ef0c834d7e7af0c3747eb30d288ecb
joaomariussi <joaomariussi10@gmail.com>
2024-01-30 11:26:27 -0300

    :rocket: Deixando o código mais simples, utilizando os includes, yield e outros métodos nas views e controllers.

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,4 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .copy('resources/css/style_basico.css', 'public/css');
+    .copy('resources/css/principal.css', 'public/css');
