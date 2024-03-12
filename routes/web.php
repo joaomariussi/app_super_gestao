90ce96e3f28f899f3a5aede8e08353a011116de3
joaomariussi <joaomariussi10@gmail.com>
2024-03-11 21:14:53 -0300

    :rocket: Ajustando as rotas.

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,29 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PrincipalController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', [PrincipalController::class, 'index'])->name('site.principal');
+    Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+    Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+    Route::post('/contato', 'ContatoController@salvar')->name('site.contato');
+    // Outras rotas que precisam de autenticação...
+});
 
-Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/login', [LoginController::class, 'index'])->name('login.index');
+Route::post('/login', [LoginController::class, 'login'])->name('login');
+Route::get('/logout', [LoginController::class, 'destroy'])->name('logout');
 
-Route::get('/contato', 'ContatoController@contato')->name('site.contato');
-
-Route::post('/contato', 'ContatoController@contato')->name('site.contato');
-
-Route::get('/login', function () { return 'Login';})->name('site.login');
-
-
-Route::prefix('/app')->group(function () {
+Route::prefix('/app')->middleware('auth')->group(function () {
     Route::get('/clientes', function () { return 'Clientes'; })->name('app.clientes');
-
     Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
-
     Route::get('/produtos', function () { return 'Produtos'; })->name('app.produtos');
 });
 
 Route::fallback(function () {
-    echo 'A rota acessada não existe. <a href="'.route('site.index').'">Clique aqui</a> para ir para a página inicial';
+    echo 'A rota acessada não existe. <a href="'.route('site.principal').'">Clique aqui</a> para ir para a página inicial';
 });
 
 
