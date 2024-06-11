b94f23bda22bb5970e7a07d533a5300b7e702911
joaomariussi <joaomariussi10@gmail.com>
2024-06-11 19:33:05 -0300

    :rocket: Ajustes nos relacionamentos das Models.
    - Cliente o relacionamento de pedido na Model do Cliente.
    
    - Fazendo o mesmo processo para a model de Fornecedor.
    
    - Ajustes nos Scripts.

--- app/Models/ClienteModel.php
+++ app/Models/ClienteModel.php
@@ -3,10 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ClienteModel extends Model
 {
     protected $table = 'clientes';
     protected $fillable = ['nome', 'cpf', 'email', 'telefone', 'endereco', 'cep', 'estado', 'cidade'];
+
+    public function pedido(): HasMany
+    {
+        return $this->hasMany(PedidoModel::class, 'cliente_id', 'id');
+    }
 
 }
--- app/Models/FornecedorModel.php
+++ app/Models/FornecedorModel.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class FornecedorModel extends Model
 {
     protected $table = 'fornecedores';
     protected $fillable = ['nome', 'site', 'uf', 'email'];
+
+    public function produto(): HasMany
+    {
+        return $this->hasMany(ProdutoModel::class, 'id_fornecedor');
+    }
 }
